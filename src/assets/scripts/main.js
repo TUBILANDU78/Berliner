15f8155feefd28c2040af9192b06329c1215ee5d
Rubén Martín Bravo <rmartinbra@uoc.edu>
2024-04-05 19:09:29 +0200

    FINAL
    FINAL

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -11,10 +11,54 @@
  */
 
 
+// USER CONTACT FORM
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const age = document.getElementById('age');
+const email = document.getElementById('email');
 
+// Show input error message
+function showError(input, message) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error';
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+}
 
-/** 
- * Header subtitle fade effect
- */
+// Show success outline
+function showSuccess(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+}
 
+// Check email is valid
+function checkEmail(input) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(re.test(input.value.trim())) {
+        showSuccess(input);
+    } else {
+        showError(input, 'El formato introducido no es válido');
+    }
+}
 
+// Check if age is valid
+function checkAge(input) {
+    const ageValue = parseInt(input.value);
+    if (isNaN(ageValue) || ageValue < 0 || ageValue >= 100) {
+        showError(input, 'La edad debe estar entre 0 y 99 años');
+    } else {
+        showSuccess(input);
+    }
+}
+
+// Get fieldName
+function getFieldName(input) {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);  
+}
+
+// Event listeners
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    checkEmail(email);
+    checkAge(age);
+});
